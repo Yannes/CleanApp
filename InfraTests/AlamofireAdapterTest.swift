eddb1f5d8b187648c21879e49dce981b72a4bfc0
Yannes Meneguelli <yannes_meneguelli@hotmail.com>
2020-04-10 15:15:17 -0300

    ensure alamofire adapter can  calls alamofire with no data

--- InfraTests/AlamofireAdapterTest.swift
+++ InfraTests/AlamofireAdapterTest.swift
@@ -20,7 +20,8 @@ class AlamofireAdapter {
     }
     
     func post(to url: URL, with data: Data?){
-        let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String:Any]
+        
+        let json =  data == nil ? nil : try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String:Any]
         session.request(url,method: .post, parameters: json, encoding: JSONEncoding.default ).resume()
     }
 }
@@ -40,13 +41,31 @@ class AlamofireAdapterTest: XCTestCase {
         sut.post(to: url, with: makeValidData())
         let exp = expectation(description: "Waiting ")
         UrlProtocolStub.observerRequest { request in
+            XCTAssertNotNil(request.httpBodyStream)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+    
+    
+    
+    
+    func test_post_should_make_request_with_no_data() throws {
+        let url = makeUrl()
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [UrlProtocolStub.self]
+        let session = Session(configuration:configuration)
+        let sut = AlamofireAdapter(session: session)
+        sut.post(to: url, with: nil)
+        let exp = expectation(description: "Waiting ")
+        UrlProtocolStub.observerRequest { request in
             XCTAssertEqual(url, request.url)
             XCTAssertEqual("POST", request.httpMethod)
             XCTAssertNotNil(request.httpBodyStream)
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1)
-    }
+    }   
 }
 
 
