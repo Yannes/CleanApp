a7b8be7fc9ff51a4d2de1dd7a05ecd4c41079f9e
Yannes Meneguelli <yannes_meneguelli@hotmail.com>
2020-04-10 12:42:02 -0300

    Add InfraLayer

---
+++ InfraTests/AlamofireAdapterTest.swift
@@ -0,0 +1,19 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Yannes Meneguelli on 10/04/20.
+//  Copyright © 2020 Yannes Meneguelli. All rights reserved.
+//
+
+import XCTest
+
+
+class AlamofireAdapterTest: XCTestCase {
+
+
+    func test_() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
