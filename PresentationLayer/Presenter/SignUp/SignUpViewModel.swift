b88fa3c4caaed0069478a46322797e325a8b34e8
Yannes Meneguelli <yannes_meneguelli@hotmail.com>
2020-04-17 19:12:15 -0300

    move and mapper var alert

---
+++ PresentationLayer/Presenter/SignUp/SignUpViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  SignUpViewModel.swift
+//  PresentationLayer
+//
+//  Created by Yannes Meneguelli on 17/04/20.
+//  Copyright © 2020 Yannes Meneguelli. All rights reserved.
+//
+
+import Foundation
+
+
+public struct SignUpViewModel{
+    public  var name: String?
+    public  var email: String?
+    public  var password: String?
+    public  var passwordConfirmation: String?
+    
+    
+    public init(name:String? = nil, email:String? = nil, password:String? = nil, passwordConfirm:String? = nil){
+        self.name = name
+        self.email = email
+        self.password = password
+        self.passwordConfirmation = passwordConfirm
+    }
+}
