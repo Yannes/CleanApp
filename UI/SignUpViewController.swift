ccc563becfb1963a379e809e8d5a2b31756ede99
Yannes Meneguelli <yannes_meneguelli@hotmail.com>
2020-04-17 20:30:39 -0300

    Include UI test

---
+++ UI/SignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewController.swift
+//  UI
+//
+//  Created by Yannes Meneguelli on 17/04/20.
+//  Copyright © 2020 Yannes Meneguelli. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+final class SignUpViewController:UIViewController {
+    
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
